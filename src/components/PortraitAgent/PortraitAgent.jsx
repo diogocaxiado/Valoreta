bac1ae503f2bc370ad0cd652ed6b6190ac3e5465
Diogo Evangelista Caxiado <65976610+DiogoCaxiado@users.noreply.github.com>
2023-07-22 01:51:51 -0300

    Aperfeiçoando a estilizaçõe do icone da classe do agente e animações

--- src/components/PortraitAgent/PortraitAgent.jsx
+++ src/components/PortraitAgent/PortraitAgent.jsx
@@ -1,13 +1,13 @@
-import "./BackgroundAgent.css";
+import "./PortraitAgent.css";
 
 export default function BackgroundAgent({ getAgentData, getAgentClass }) {
   return (
     <div className="agent">
-      <div className="border-background-agent">
-        <img className="background-agent" src={getAgentClass()} />
+      <div className="border-class">
+        <img className="icon-class" src={getAgentClass("displayIcon")} />
       </div>
       <img
-        className="agent-body"
+        className={"agent-body"}
         src={getAgentData("fullPortrait")}
         alt="agent photo"
       />
